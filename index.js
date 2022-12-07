cbc18edb2f3bf7a4ba30d14f50460a3567e4e9ea
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-07 11:06:53 -0600

    Connect static html to express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+// Make a web server with Express
+// hosts static content, same content for all requests, and all users
+
+const express = require('express'); // load express
+const app = express(); // Initialize the express app
+const port = 3000;
+
+app.use(express.static('public')); // allows express to read the 'public' folder
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
